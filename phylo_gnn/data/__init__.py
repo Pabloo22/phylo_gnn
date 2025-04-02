d93fb45e95fb10a64498e8cf3e7aff4bdf8f3f36
Pabloo22 <pablete.arino@gmail.com>
2025-04-02 15:46:51 +0200

    Add DEFAULT_LABEL_NAMES to data module exports

--- phylo_gnn/data/__init__.py
+++ phylo_gnn/data/__init__.py
@@ -1,6 +1,8 @@
-from ._phylo_dataset import CSVMetadata, PhyloCSVDataset
+from ._phylo_dataset import CSVMetadata, PhyloCSVDataset, DEFAULT_LABEL_NAMES
+
 
 __all__ = [
     "CSVMetadata",
     "PhyloCSVDataset",
+    "DEFAULT_LABEL_NAMES",
 ]
