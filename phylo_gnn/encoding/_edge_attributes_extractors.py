84daaf0e4b9f4e89b8f840557591a440cdf4491c
Pabloo22 <pablete.arino@gmail.com>
2025-04-02 19:31:00 +0200

    Add composite edge feature extractor to concatenate edge features from multiple extractors

--- phylo_gnn/encoding/_edge_attributes_extractors.py
+++ phylo_gnn/encoding/_edge_attributes_extractors.py
@@ -1,7 +1,9 @@
+from collections.abc import Iterable
+from collections import defaultdict
 from numpy.typing import NDArray
 import numpy as np
 
-from phylo_gnn.encoding import VectorTree
+from phylo_gnn.encoding import VectorTree, EdgeFeatureExtractor
 
 
 def get_distances(
@@ -24,11 +26,7 @@ def get_topological_distances(
     vector_tree: VectorTree,
     edge_indices: dict[tuple[str, str, str], NDArray[np.int64]],
 ) -> dict[tuple[str, str, str], NDArray[np.float32]]:
-    """Returns the topological distance between the nodes in the edge index.
-
-    This function can be used to get edge attributes for the edges in the
-    edge index.
-    """
+    """Returns the topological distance between the nodes in the edge index."""
     result = {}
     for edge_type, edge_index in edge_indices.items():
         distances = vector_tree.get_topological_distances(
@@ -36,3 +34,30 @@ def get_topological_distances(
         )
         result[edge_type] = distances
     return result
+
+
+def composite_edge_feature_extractor(
+    extractors: Iterable[EdgeFeatureExtractor],
+) -> EdgeFeatureExtractor:
+    """Concatenates the edge features from the extractors."""
+
+    def concat(
+        vector_tree: VectorTree,
+        edge_indices: dict[tuple[str, str, str], NDArray[np.int64]],
+    ) -> dict[tuple[str, str, str], NDArray[np.float32]]:
+        features: dict[tuple[str, str, str], list[NDArray[np.float32]]] = (
+            defaultdict(list)
+        )
+        for extractor in extractors:
+            edge_features = extractor(vector_tree, edge_indices)
+            for edge_type, feature in edge_features.items():
+                if feature.ndim == 1:
+                    feature = feature[:, np.newaxis]
+                features[edge_type].append(feature)
+        features_combined = {
+            edge_type: np.concatenate(features[edge_type], axis=1)
+            for edge_type in features
+        }
+        return features_combined
+
+    return concat
