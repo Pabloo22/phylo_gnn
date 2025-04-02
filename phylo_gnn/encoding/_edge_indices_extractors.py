3ca3fcba1fc0ddb120899e5b95522b78a949541e
Pabloo22 <pablete.arino@gmail.com>
2025-04-02 18:52:48 +0200

    Add edge index mapping to get_basic_edge_index function in _edge_indices_extractors

--- phylo_gnn/encoding/_edge_indices_extractors.py
+++ phylo_gnn/encoding/_edge_indices_extractors.py
@@ -4,3 +4,16 @@ from phylo_gnn.encoding import VectorTree, EdgeType, NodeType
 def get_basic_edge_index(vector_tree: VectorTree):
     """Returns (node, has_parent, node) and (node, has_child, node) edge
     indices types."""
+
+    return {
+        (
+            NodeType.NODE.value,
+            EdgeType.HAS_PARENT.value,
+            NodeType.NODE.value,
+        ): vector_tree.children_to_parent_edge_index,
+        (
+            NodeType.NODE.value,
+            EdgeType.HAS_CHILD.value,
+            NodeType.NODE.value,
+        ): vector_tree.parent_to_children_edge_index,
+    }
