f5eeaa99398ebcbc1324ece7dd86522b1f074190
Pabloo22 <pablete.arino@gmail.com>
2025-04-03 18:49:48 +0200

    Add get_mlp utility to model module exports

--- phylo_gnn/model/__init__.py
+++ phylo_gnn/model/__init__.py
@@ -2,6 +2,7 @@ from ._utils import (
     get_node_features_dict,
     get_edge_attributes_dict,
     get_edge_indices_dict,
+    get_mlp,
 )
 
 from ._phylo_gnn import PhyloGNNModule
@@ -10,5 +11,6 @@ __all__ = [
     "get_node_features_dict",
     "get_edge_attributes_dict",
     "get_edge_indices_dict",
+    "get_mlp",
     "PhyloGNNModule",
 ]
