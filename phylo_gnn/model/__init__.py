8a804a7af5d1237a26cd0d90ab1756fffa4d5f73
Pabloo22 <pablete.arino@gmail.com>
2025-04-03 13:15:14 +0200

    Update __init__.py exports to include BaseMessagePassing and BaseReadout classes

--- phylo_gnn/model/__init__.py
+++ phylo_gnn/model/__init__.py
@@ -1,9 +1,14 @@
 from ._periodic_encoder import MultiPeriodicEncoder
 from ._utils import get_node_features_dict, get_edge_attributes_dict
 from ._encoders import BaseEncoder
+from ._message_passing import BaseMessagePassing
+from ._readouts import BaseReadout
 
 __all__ = [
     "MultiPeriodicEncoder",
     "get_node_features_dict",
     "get_edge_attributes_dict",
+    "BaseEncoder",
+    "BaseMessagePassing",
+    "BaseReadout",
 ]
