edcd6263adc4c8a6f7fbfae42105762d0e30ce84
Pabloo22 <pablete.arino@gmail.com>
2025-04-03 13:00:42 +0200

    Add BaseMessagePassing class for message passing framework

---
+++ phylo_gnn/model/_message_passing.py
@@ -0,0 +1,57 @@
+import abc
+from typing import overload
+import torch
+from torch import nn
+
+
+class BaseMessagePassing(nn.Module):
+    """Base class for message passing"""
+
+    def __init__(
+        self,
+        node_input_dims: dict[str, int],
+        node_output_dims: dict[str, int] | int,
+        edge_input_dims: dict[tuple[str, str, str], int] | None = None,
+        edge_output_dims: dict[tuple[str, str, str], int] | int | None = None,
+    ):
+        super().__init__()
+        self.node_input_dims = node_input_dims
+        self.node_output_dims = node_output_dims
+        self.edge_input_dims = edge_input_dims
+        self.edge_output_dims = edge_output_dims
+
+    @overload
+    def forward(
+        self,
+        node_features_dict: dict[str, torch.Tensor],
+        edge_indices_dict: dict[tuple[str, str, str], torch.Tensor],
+        edge_attributes_dict: None = None,
+    ) -> tuple[
+        dict[str, torch.Tensor],
+        None,
+    ]: ...
+
+    @overload
+    def forward(
+        self,
+        node_features_dict: dict[str, torch.Tensor],
+        edge_indices_dict: dict[tuple[str, str, str], torch.Tensor],
+        edge_attributes_dict: dict[tuple[str, str, str], torch.Tensor],
+    ) -> tuple[
+        dict[str, torch.Tensor],
+        dict[tuple[str, str, str], torch.Tensor],
+    ]: ...
+
+    @abc.abstractmethod
+    def forward(
+        self,
+        node_features_dict: dict[str, torch.Tensor],
+        edge_indices_dict: dict[tuple[str, str, str], torch.Tensor],
+        edge_attributes_dict: (
+            dict[tuple[str, str, str], torch.Tensor] | None
+        ) = None,
+    ) -> tuple[
+        dict[str, torch.Tensor],
+        dict[tuple[str, str, str], torch.Tensor] | None,
+    ]:
+        pass
