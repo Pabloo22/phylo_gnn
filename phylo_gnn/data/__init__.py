182dbe57799687cffa0213137f2863b6cfcf1cc4
Pabloo22 <pablete.arino@gmail.com>
2025-04-05 23:39:48 +0200

    Add utility functions for data splitting and loading to the data module

--- phylo_gnn/data/__init__.py
+++ phylo_gnn/data/__init__.py
@@ -1,9 +1,14 @@
 from ._csv_metadata import CSVMetadata, DEFAULT_LABEL_NAMES
 from ._phylo_dataset import PhyloCSVDataset
-
+from ._utils import (
+    create_data_splits,
+    create_data_loaders,
+)
 
 __all__ = [
     "CSVMetadata",
     "PhyloCSVDataset",
     "DEFAULT_LABEL_NAMES",
+    "create_data_splits",
+    "create_data_loaders",
 ]
