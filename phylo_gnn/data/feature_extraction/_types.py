583b7545a241f3b351a1fb30e25f55b039957ecf
Pabloo22 <pablete.arino@gmail.com>
2025-04-05 17:10:43 +0200

    Add type definitions and feature pipeline classes for feature extraction

--- phylo_gnn/data/feature_extraction/_types.py
+++ phylo_gnn/data/feature_extraction/_types.py
@@ -1,26 +1,29 @@
 from collections.abc import Callable
 import enum
+from typing import NamedTuple
 
 import torch
 from torch_geometric.data import HeteroData  # type: ignore
+from torch_geometric.typing import EdgeType  # type: ignore
 import numpy as np
 from numpy.typing import NDArray
 
-from phylo_gnn.feature_extraction import VectorTree
+from phylo_gnn.data.feature_extraction import VectorTree
 
 
-EdgeIndices = dict[tuple[str, str, str], NDArray[np.int64]]
+EdgeIndices = dict[EdgeType, NDArray[np.int64]]
 EncodingFunction = Callable[[str, NDArray], HeteroData]
 NodeFeatureExtractor = Callable[[VectorTree], dict[str, NDArray[np.float32]]]
 EdgeIndicesExtractor = Callable[[VectorTree], EdgeIndices]
 EdgeFeatureExtractor = Callable[
-    [VectorTree, EdgeIndices], dict[tuple[str, str, str], NDArray[np.float32]]
+    [VectorTree, EdgeIndices], dict[EdgeType, NDArray[np.float32]]
 ]
 TargetProcessor = (
     Callable[[NDArray], torch.Tensor]
     | Callable[[NDArray], dict[str, torch.Tensor]]
-    | Callable[[NDArray], dict[tuple[str, str, str], torch.Tensor]]
+    | Callable[[NDArray], dict[EdgeType, torch.Tensor]]
 )
+NormalizationFunction = Callable[[NDArray, VectorTree], NDArray[np.float32]]
 
 
 class NodeNames(str, enum.Enum):
@@ -36,3 +39,8 @@ class EdgeNames(str, enum.Enum):
     HAS_ANCESTOR = "has_ancestor"
     HAS_DESCENDANT = "has_descendant"
     HAS_ADJACENT_LEVEL_POSITION = "has_adjacent_level_position"
+
+
+class FeaturePipeline(NamedTuple):
+    feature_name: str
+    normalization_fn_name: str | None
