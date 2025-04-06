cca19ad0c636f7b60e9e81a7fd6eb252eb62bdf3
Pabloo22 <pablete.arino@gmail.com>
2025-04-06 19:16:35 +0200

    Add dataset configuration to experiment setup in experiment_configs.py

--- phylo_gnn/config/experiment_configs.py
+++ phylo_gnn/config/experiment_configs.py
@@ -1,7 +1,17 @@
 from phylo_gnn.config import (
     Config,
     TrainingConfig,
+    PhyloCSVDatasetConfig,
+    CSVMetadataConfig,
 )
 
 
-EXPERIMENT_1 = Config(training_config=TrainingConfig(run_name="experiment_1"))
+EXPERIMENT_1 = Config(
+    training_config=TrainingConfig(run_name="experiment_1"),
+    dataset=PhyloCSVDatasetConfig(
+        csv_metadata_config=CSVMetadataConfig(
+            csv_filenames=["87_10k_nwk_subset_1000.csv"],
+            processed_filename="87_10k_nwk_subset_1000",
+        ),
+    ),
+)
