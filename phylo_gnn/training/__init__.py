d18683ee902808ea719413d3907ea9e3fedc636c
Pabloo22 <pablete.arino@gmail.com>
2025-04-06 18:52:35 +0200

    Add training functions to the training module for improved training execution

--- phylo_gnn/training/__init__.py
+++ phylo_gnn/training/__init__.py
@@ -3,10 +3,12 @@ from ._utils import (
     configure_callbacks,
     initialize_trainer,
 )
-
+from ._train import train, execute_training
 
 __all__ = [
     "configure_logger",
     "configure_callbacks",
     "initialize_trainer",
+    "train",
+    "execute_training",
 ]
