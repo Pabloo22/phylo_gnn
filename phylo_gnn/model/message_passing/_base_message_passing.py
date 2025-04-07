9c89bbbc1be5ff3d2fe093f5bb072fed9a49f17e
Pabloo22 <pablete.arino@gmail.com>
2025-04-07 11:13:03 +0200

    Add edge_types parameter to BaseMessagePassing and implement edge_types property

--- phylo_gnn/model/message_passing/_base_message_passing.py
+++ phylo_gnn/model/message_passing/_base_message_passing.py
@@ -12,6 +12,7 @@ class BaseMessagePassing(nn.Module):
         self,
         node_input_dims: dict[str, int],
         node_output_dims: dict[str, int] | int,
+        edge_types: list[EdgeType] | None = None,
         edge_input_dims: dict[EdgeType, int] | None = None,
         edge_output_dims: dict[EdgeType, int] | int | None = None,
         **kwargs,
@@ -19,11 +20,23 @@ class BaseMessagePassing(nn.Module):
         super().__init__()
         self.node_input_dims = node_input_dims
         self.node_output_dims = node_output_dims
+        self._edge_types = edge_types
         self.edge_input_dims = edge_input_dims
         self.edge_output_dims = edge_output_dims
         kwargs_copy = kwargs.copy()
         kwargs_copy.pop("nn", None)
         self.hparams = kwargs_copy
+
+    @property
+    def edge_types(self) -> list[EdgeType]:
+        """Returns the edge types."""
+        if self._edge_types is None:
+            if self.edge_input_dims is None:
+                raise ValueError(
+                    "edge_input_dims must be provided if edge_types is None."
+                )
+            self._edge_types = list(self.edge_input_dims.keys())
+        return self._edge_types
 
     @overload
     def forward(
