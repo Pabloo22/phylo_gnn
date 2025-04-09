4a28ee69f7d6169386d8081add06f5bb467378b2
Pabloo22 <pablete.arino@gmail.com>
2025-04-09 13:09:31 +0200

    Add debug configuration for level-based node and edge attributes

--- phylo_gnn/config/experiment_configs.py
+++ phylo_gnn/config/experiment_configs.py
@@ -5,8 +5,15 @@ from phylo_gnn.config import (
     CSVMetadataConfig,
     PhyloGNNClassifierConfig,
     MessagePassingConfig,
+    ProcessFunctionConfig,
+    ReadoutConfig,
+    FeatureEncoderConfig,
 )
-
+from phylo_gnn.config import (
+    node_features_with_level_nodes,
+    edge_attributes_with_level_nodes,
+)
+from phylo_gnn.data.feature_extraction import NodeNames, EdgeNames
 
 DEBUG = Config(
     training_config=TrainingConfig(run_name="debug"),
@@ -96,6 +103,60 @@ EXPERIMENT_3 = Config(
                 "layer_norm": False,
                 "dropout": 0.0,
             }
+        ),
+        scheduler=None,
+        learning_rate=0.0003,
+        weight_decay=0.00001,
+    ),
+)
+DEBUG_3 = Config(
+    training_config=TrainingConfig(
+        run_name="debug_level_nodes",
+        patience=40,
+        num_workers=1,
+        max_epochs=100,
+    ),
+    dataset=PhyloCSVDatasetConfig(
+        process_function_config=ProcessFunctionConfig(
+            node_features=node_features_with_level_nodes(),
+            edge_attributes=edge_attributes_with_level_nodes(),
+            edge_types=list(edge_attributes_with_level_nodes().keys()),
+        ),
+        csv_metadata_config=CSVMetadataConfig(
+            csv_filenames=["87_10k_nwk_subset_1000.csv"],
+            processed_filename="test_level_nodes",
+        ),
+    ),
+    model=PhyloGNNClassifierConfig(
+        encoder=FeatureEncoderConfig(
+            node_output_dims={
+                NodeNames.LEVEL.value: 96,
+                NodeNames.NODE.value: 28,
+            },
+            edge_output_dims=4,
+        ),
+        message_passing=MessagePassingConfig(
+            parameters={
+                "layer_norm": False,
+                "dropout": 0.0,
+            },
+            node_output_dims={
+                NodeNames.LEVEL.value: 96,
+                NodeNames.NODE.value: 28,
+            },
+            edge_output_dims=4,
+        ),
+        readout=ReadoutConfig(
+            parameters={
+                "node_types_to_use": ["level"],
+                "edge_attributes_to_use": [
+                    (
+                        NodeNames.NODE.value,
+                        EdgeNames.HAS_PARENT.value,
+                        NodeNames.NODE.value,
+                    )
+                ],
+            },
         ),
         scheduler=None,
         learning_rate=0.0003,
